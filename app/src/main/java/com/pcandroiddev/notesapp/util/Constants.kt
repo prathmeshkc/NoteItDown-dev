b87aaf235b0a40c6aa90005dd7d44b37f6512e51
prathmeshkc <prathmeshkc2001@gmail.com>
2022-12-16 16:10:07 -0500

    changed base url

--- app/src/main/java/com/pcandroiddev/notesapp/util/Constants.kt
+++ app/src/main/java/com/pcandroiddev/notesapp/util/Constants.kt
@@ -2,9 +2,7 @@ package com.pcandroiddev.notesapp.util
 
 object Constants {
     const val TAG = "r3tr0"
-    const val BASE_URL = "https://notesserviceapi.herokuapp.com/"
+    const val BASE_URL = "https://noteitdownservice.onrender.com/"
     const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
     const val USER_TOKEN = "USER_TOKEN"
-
-
 }
