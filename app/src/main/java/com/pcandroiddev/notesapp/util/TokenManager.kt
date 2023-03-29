4a8451bcd0f2bac29308115639fe996b85b1fbd4
prathmeshkc <prathmeshkc2001@gmail.com>
2023-03-29 01:14:32 -0400

    -added logout feature

--- app/src/main/java/com/pcandroiddev/notesapp/util/TokenManager.kt
+++ app/src/main/java/com/pcandroiddev/notesapp/util/TokenManager.kt
@@ -1,6 +1,7 @@
 package com.pcandroiddev.notesapp.util
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.pcandroiddev.notesapp.util.Constants.PREFS_TOKEN_FILE
 import com.pcandroiddev.notesapp.util.Constants.USER_TOKEN
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -8,15 +9,20 @@ import javax.inject.Inject
 
 class TokenManager @Inject constructor(@ApplicationContext context: Context) {
     private var prefs = context.getSharedPreferences(PREFS_TOKEN_FILE, Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = prefs.edit()
 
     fun saveToken(token: String) {
-        val editor = prefs.edit()
         editor.putString(USER_TOKEN, token)
         editor.apply()
     }
 
     fun getToken(): String? {
         return prefs.getString(USER_TOKEN, null)
+    }
+
+    fun deleteToken() {
+        editor.remove(USER_TOKEN)
+        editor.apply()
     }
 
 
